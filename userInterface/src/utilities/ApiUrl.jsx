5c99a44029f8aea7f6dfb9a9b3b8bf0052cf05a0
Mahmodul Hasan <nicesanto92@gmail.com>
2023-06-16 12:57:13 +0600

    patch links

--- userInterface/src/utilities/ApiUrl.jsx
+++ userInterface/src/utilities/ApiUrl.jsx
@@ -1,2 +1,2 @@
-export const MOVIE_API = import.meta.env.VITE_MOVIE_API;
-export const THE_MOVIE_DB_URL = import.meta.env.VITE_MOVIEDB_URL;
+export const MOVIE_API = `${import.meta.env.VITE_MOVIE_API}`;
+export const THE_MOVIE_DB_URL = `${import.meta.env.VITE_MOVIEDB_URL}`;
